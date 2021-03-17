7c23f2d479c524e5b54e5cfaaaeedda65ab88686
Doychin Ivanov <doychinivn@gmail.com>
2021-03-17 21:34:49 +0200

    Update to 'numbersFromOneToTen'
    Hello, my dear flame of Udun,
    
    I present to your attention an improvement of your solution!
    
    I have made your function abstract, so that one does not need to type all the numbers they want to return. To achieve that I used closure and recursion.  Feel free to either approve or disapprove my update. 
    
    ...run you fool!

--- 1-js-basics/1-first-steps-in-coding-lab/numbersFromOneToTen.js
+++ 1-js-basics/1-first-steps-in-coding-lab/numbersFromOneToTen.js
@@ -1,5 +1,22 @@
-function solve() {
+function getNumbersInRange(n, m){
+    const result = [];
+    return setNumbers(n, m)
+    .join('\n');
 
-    return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].join('\n');
+    function setNumbers(n, m) {
+        if (n <= m) {
+          result.push(n);
+          n++;
+          setNumbers(n, m);
+        }
+
+        return result;
+    }
 
 }
+
+console.log(getNumbersInRange(1, 10));
+
+// Will print a diffrent range of numbers
+// console.log(getNumbersInRange(1, 15));
+// console.log(getNumbersInRange(1, 20));
