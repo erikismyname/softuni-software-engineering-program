2bd339a2a34db5a8ff096e23d28905b05d326077
Erik Ivanov <78368815+erikismyname@users.noreply.github.com>
2021-03-18 11:21:03 +0200

    Update numbersFromOneToTen.js

--- 1-js-basics/1-first-steps-in-coding-lab/numbersFromOneToTen.js
+++ 1-js-basics/1-first-steps-in-coding-lab/numbersFromOneToTen.js
@@ -1,16 +1,23 @@
 function getNumbersInRange(n, m){
+    
     const result = [];
-    return setNumbers(n, m)
-    .join('\n');
+    
+    return setNumbers(n, m).join('\n');
 
     function setNumbers(n, m) {
+        
         if (n <= m) {
+            
           result.push(n);
+            
           n++;
+            
           setNumbers(n, m);
+            
         }
 
         return result;
+        
     }
 
 }
