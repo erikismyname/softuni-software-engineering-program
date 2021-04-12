82db923d9a8651398ed49a09e325ef80efedca95
erikivanov7 <advictoriam@abv.bg>
2021-04-12 16:20:51 +0300

    Added a new solution

---
+++ 1-js-basics/3.1-conditional-statements-advanced-lab/cinemaTicket.js
@@ -0,0 +1,23 @@
+function solve([day]) {
+
+    let priceData = {
+
+        'Monday': 12,
+
+        'Tuesday': 12,
+
+        'Wednesday' : 14,
+
+        'Thursday': 14,
+
+        'Friday': 12,
+
+        'Saturday': 16,
+
+        'Sunday': 16
+
+    }
+
+    return priceData[day];
+
+}
