c6f20bd28f22015fc9e28e241821680e461a60a8
erikivanov7 <advictoriam@abv.bg>
2021-04-24 10:03:35 +0300

    Added a new solution

---
+++ 2-js-fundamentals/1.1-basic-syntax-conditional-statements-and-loops-lab/06-studentInformation.js
@@ -0,0 +1,7 @@
+function solve(name, age, grade) {
+
+    return `Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`;
+
+}
+
+console.log(solve('John', 15, 5.54678)); // Name: John, Age: 15, Grade: 5.55
