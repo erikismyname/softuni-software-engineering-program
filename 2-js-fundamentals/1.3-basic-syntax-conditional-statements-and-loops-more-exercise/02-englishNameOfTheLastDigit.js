2388b1ac6f103fec7fd96652878ba0cb385891c1
erikivanov7 <advictoriam@abv.bg>
2021-05-07 13:24:35 +0300

    Added a new solution

---
+++ 2-js-fundamentals/1.3-basic-syntax-conditional-statements-and-loops-more-exercise/02-englishNameOfTheLastDigit.js
@@ -0,0 +1,33 @@
+function solve(num) {
+
+    let data = {
+
+        1: 'one',
+
+        2: 'two',
+
+        3: 'three',
+
+        4: 'four',
+
+        5: 'five',
+
+        6: 'six',
+
+        7: 'seven',
+
+        8: 'eight',
+
+        9: 'nine',
+
+        0: 'zero'
+
+    };
+
+    num = num.toString();
+
+    return data[num[num.length - 1]];
+
+}
+
+console.log(solve(512)); // two
