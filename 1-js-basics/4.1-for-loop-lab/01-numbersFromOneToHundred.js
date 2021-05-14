7543d0fd7f890d696b3118dd0a571552139feb1d
erikivanov7 <advictoriam@abv.bg>
2021-05-14 09:06:15 +0300

    Added a new solution

---
+++ 1-js-basics/4.1-for-loop-lab/01-numbersFromOneToHundred.js
@@ -0,0 +1,15 @@
+function solve() {
+
+    let result = [];
+
+    for (let a = 1; a <= 100; a++) {
+
+        result.push(a);
+
+    }
+
+    return result.join('\n');
+
+}
+
+console.log(solve());
