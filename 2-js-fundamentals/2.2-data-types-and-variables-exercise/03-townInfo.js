0e1abbf791b9400e142d8325c65e5cae9bf183fd
erikivanov7 <advictoriam@abv.bg>
2021-06-17 09:15:10 +0300

    Added a new solution

---
+++ 2-js-fundamentals/2.2-data-types-and-variables-exercise/03-townInfo.js
@@ -0,0 +1,7 @@
+function solve(name, pop, area) {
+
+    return `Town ${name} has population of ${pop} and area ${area} square km.`;
+
+}
+
+console.log(solve('Sofia', 1286383, 492)); // Town Sofia has population of 1286383 and area 492 square km.
