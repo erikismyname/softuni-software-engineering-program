ef4ed74cff7948cc31a3162bf7a7a699d17f010d
erikivanov7 <advictoriam@abv.bg>
2021-06-19 10:27:26 +0300

    Added a new solution

---
+++ 2-js-fundamentals/2.3-data-types-and-variables-more-exercise/01-digitsWithWords.js
@@ -0,0 +1,31 @@
+function solve(str) {
+
+    let data = {
+
+        'one': 1,
+
+        'two': 2,
+
+        'three': 3,
+
+        'four': 4,
+
+        'five': 5,
+
+        'six': 6,
+
+        'seven': 7,
+
+        'eight': 8,
+
+        'nine': 9,
+
+        'zero': 0
+
+    };
+
+    return data[str];
+
+}
+
+console.log(solve('nine')); // 9
