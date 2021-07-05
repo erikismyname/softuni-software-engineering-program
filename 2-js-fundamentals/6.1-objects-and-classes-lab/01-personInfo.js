5e26bce695f8ff26c5d46e412830678bd28e07cc
erikivanov7 <advictoriam@abv.bg>
2021-07-05 10:30:16 +0300

    Added a new solution

---
+++ 2-js-fundamentals/6.1-objects-and-classes-lab/01-personInfo.js
@@ -0,0 +1,15 @@
+function solve(firstName, lastName, age) {
+
+    return {
+
+        firstName,
+
+        lastName,
+
+        age
+
+    };
+
+}
+
+console.log(solve('Peter', 'Pan', '20')); // { firstName: 'Peter', lastName: 'Pan', age: '20' }
