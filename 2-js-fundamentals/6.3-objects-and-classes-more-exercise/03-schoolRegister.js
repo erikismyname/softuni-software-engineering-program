09e3c75274fb5cd1e4d5d596587c87f0f508fb5c
erikivanov7 <advictoriam@abv.bg>
2021-07-09 12:48:34 +0300

    Added a new solution

---
+++ 2-js-fundamentals/6.3-objects-and-classes-more-exercise/03-schoolRegister.js
@@ -0,0 +1,36 @@
+function solve(arr) {
+
+    
+
+}
+
+console.log(solve(["Student name: Mark, Grade: 8, Graduated with an average score: 4.75",
+    "Student name: Ethan, Grade: 9, Graduated with an average score: 5.66",
+    "Student name: George, Grade: 8, Graduated with an average score: 2.83",
+    "Student name: Steven, Grade: 10, Graduated with an average score: 4.20",
+    "Student name: Joey, Grade: 9, Graduated with an average score: 4.90",
+    "Student name: Angus, Grade: 11, Graduated with an average score: 2.90",
+    "Student name: Bob, Grade: 11, Graduated with an average score: 5.15",
+    "Student name: Daryl, Grade: 8, Graduated with an average score: 5.95",
+    "Student name: Bill, Grade: 9, Graduated with an average score: 6.00",
+    "Student name: Philip, Grade: 10, Graduated with an average score: 5.05",
+    "Student name: Peter, Grade: 11, Graduated with an average score: 4.88",
+    "Student name: Gavin, Grade: 10, Graduated with an average score: 4.00"]
+));
+/*
+9 Grade
+List of students: Mark, Daryl
+Average annual grade from last year: 5.35
+
+10 Grade
+List of students: Ethan, Joey, Bill
+Average annual grade from last year: 5.52
+
+11 Grade
+List of students: Steven, Philip, Gavin
+Average annual grade from last year: 4.42
+
+12 Grade
+List of students: Bob, Peter
+Average annual grade from last year: 5.02
+*/
