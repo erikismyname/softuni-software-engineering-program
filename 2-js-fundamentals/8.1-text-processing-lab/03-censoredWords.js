659266ffc70736ce9e840161472ce7de175f9bb1
erikivanov7 <advictoriam@abv.bg>
2021-07-13 11:23:47 +0300

    Added a new solution

---
+++ 2-js-fundamentals/8.1-text-processing-lab/03-censoredWords.js
@@ -0,0 +1,13 @@
+function solve(text, word) {
+
+    while (text.includes(word)) {
+
+        text = text.replace(word, '*'.repeat(word.length));
+
+    }
+
+    return text;
+
+}
+
+console.log(solve('A small sentence with some words', 'small'));
