e8fcd8f595fce053a1f36cba0228edd4c28d9a2d
erikivanov7 <advictoriam@abv.bg>
2021-07-31 10:42:54 +0300

    Added a new solution

---
+++ 3-js-advanced/2.1-arrays-and-nested-arrays-lab/10-diagonalSums.js
@@ -0,0 +1,17 @@
+function solve(matrix) {
+
+    let mainDiag = 0;
+
+    let secDiag = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+
+        mainDiag += matrix[i][i];
+
+        secDiag += matrix[i][matrix.length - 1 - i];
+
+    }
+
+    return `${mainDiag} ${secDiag}`;
+
+}
