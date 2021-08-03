4645f37aeb3ceaad2670192820e9b7754d8f2e61
erikivanov7 <advictoriam@abv.bg>
2021-08-03 11:44:35 +0300

    Added a new solution

---
+++ 3-js-advanced/3.1-objects-and-composition-lab/04-objectFactory.js
@@ -0,0 +1,52 @@
+function solve(funcLibrary, ordersArr) {
+
+    const result = [];
+
+    ordersArr.forEach(e => {
+
+        const obj = Object.assign({}, e.template);
+
+        e.parts.forEach(p => {
+
+            obj[p] = funcLibrary[p];
+
+        });
+
+        result.push(obj);
+
+    });
+
+    return result;
+
+}
+
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
+
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    },
+];
