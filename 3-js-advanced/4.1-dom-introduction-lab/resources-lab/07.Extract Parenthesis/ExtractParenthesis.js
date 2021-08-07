3aa982f60bcf67f8e8f7ef8b90adcea17d558032
erikivanov7 <advictoriam@abv.bg>
2021-08-07 16:50:38 +0300

    Added a new solution

---
+++ 3-js-advanced/4.1-dom-introduction-lab/resources-lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -0,0 +1,21 @@
+function extract(content) {
+
+    const text = document.getElementById(content).textContent;
+
+    const result = [];
+
+    const pattern = /\(.+?\)/g;
+
+    let match = pattern.exec(text);
+
+    while (match) {
+
+        result.push(match[0].slice(1, -1));
+
+        match = pattern.exec(text);
+
+    }
+
+    return result.join('; ');
+
+}
