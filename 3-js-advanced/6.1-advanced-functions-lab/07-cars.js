d8e97cbfce3dfb29e953495e9d4221c33c3b67b4
erikivanov7 <advictoriam@abv.bg>
2021-08-14 10:31:06 +0300

    Added a new solution

---
+++ 3-js-advanced/6.1-advanced-functions-lab/07-cars.js
@@ -0,0 +1,51 @@
+function processData(arr) {
+
+    const data = {};
+
+    const methods = {
+
+        create([model, ...rest]) {
+
+            !rest.length ? data[model] = {} : data[model] = Object.create(data[rest[1]]);
+
+        },
+
+        set([model, key, value]) {
+
+            data[model][key] = value;
+
+        },
+
+        print([name]) {
+
+            const result = [];
+
+            for (let key in data[name]) {
+
+                result.push(`${key}:${data[name][key]}`);
+
+            }
+
+            console.log(result.join(', '));
+
+        },
+
+    };
+
+    arr.forEach(e => {
+
+        const [command, ...values] = e.split(' ');
+
+        methods[command](values);
+
+    });
+
+}
+
+processData(['create c1',
+    'create c2 inherit c1',
+    'set c1 color red',
+    'set c2 model new',
+    'print c1',
+    'print c2']
+);
