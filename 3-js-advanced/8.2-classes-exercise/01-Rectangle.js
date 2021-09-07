82935d04eae9ff676b707da6b9342e0639a28c19
erikivanov7 <advictoriam@abv.bg>
2021-09-07 15:43:10 +0300

    Added a new solution

---
+++ 3-js-advanced/8.2-classes-exercise/01-Rectangle.js
@@ -0,0 +1,19 @@
+class Rectangle {
+
+    constructor(width, height, color) {
+
+        this.width = width;
+
+        this.height = height;
+
+        this.color = color;
+
+    }
+
+    calcArea() {
+
+        return this.width * this.height;
+
+    }
+
+}
