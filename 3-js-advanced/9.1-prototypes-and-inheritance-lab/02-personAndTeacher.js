609994a25d6914ca20d329121cf3519a8e5dec68
erikivanov7 <advictoriam@abv.bg>
2021-09-10 12:43:57 +0300

    Added a new solution

---
+++ 3-js-advanced/9.1-prototypes-and-inheritance-lab/02-personAndTeacher.js
@@ -0,0 +1,29 @@
+function composeClasses() {
+
+    class Person {
+
+        constructor(name, email) {
+
+            this.name = name;
+
+            this.email = email;
+
+        }
+
+    }
+
+    class Teacher extends Person {
+
+        constructor(name, email, subject) {
+
+            super(name, email);
+
+            this.subject = subject;
+
+        }
+
+    }
+
+    return { Person, Teacher };
+
+}
