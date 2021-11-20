e77968e85f5633a7eb7f1b6594f86bd25d350e9f
erikivanov7 <advictoriam@abv.bg>
2021-11-20 17:04:19 +0200

    Added a new resource

---
+++ 5-js-back-end/6-mongodb-and-mongoose-workshop/src/models/Comment.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const commentSchema = new Schema({
+    author: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+});
+
+const Comment = model('Comment', commentSchema);
+
+module.exports = Comment;
---
+++ 5-js-back-end/6-mongodb-and-mongoose-workshop/src/models/Cube.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const cubeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 500
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        match: /^https?:\/\//,
+    },
+    difficulty: {
+        type: Number,
+        min: 1,
+        max: 6,
+    },
+    comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+    accessories: [{ type: Schema.Types.ObjectId, ref: 'Accessory' }],
+});
+
+const Cube = model('Cube', cubeSchema);
+
+module.exports = Cube;
