8bbfd0bdea1b27e8791ad60c629731714f9662ca
erikivanov7 <advictoriam@abv.bg>
2021-12-24 17:06:32 +0200

    Added a new resource

---
+++ 5-js-back-end/12-exam-practice/5-shared-trip/solution/src/config/constantsConfig.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: 3000,
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017a/shared-trip-solo',
+    TOKEN_SECRET: 'secret',
+    COOKIE_NAME: 'SESSION_TOKEN',
+    SALT_ROUNDS: 10,
+};
