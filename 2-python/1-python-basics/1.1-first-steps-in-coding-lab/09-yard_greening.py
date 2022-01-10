e480bef27fde9f518e07a941b0e46d4c234895a8
erikivanov7 <advictoriam@abv.bg>
2022-01-10 21:10:11 +0200

    Modified ninth solution

--- 2-python/1-python-basics/1.1-first-steps-in-coding-lab/09-yard_greening.py
+++ 2-python/1-python-basics/1.1-first-steps-in-coding-lab/09-yard_greening.py
@@ -1,4 +1,6 @@
-total_square_m_price = float(input()) * 7.61
+total_square_m = float(input())
+
+total_square_m_price = total_square_m * 7.61
 
 print(f'The final price is: {total_square_m_price * 0.82} lv.')
 
