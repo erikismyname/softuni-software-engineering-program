1969c8f7bdc22ae2bc451d2bc5ff6a75ee549e53
erikivanov7 <advictoriam@abv.bg>
2022-01-11 19:28:27 +0200

    Modified eighth solution

--- 2-python/1-python-basics/1.1-first-steps-in-coding-lab/08-pet_shop.py
+++ 2-python/1-python-basics/1.1-first-steps-in-coding-lab/08-pet_shop.py
@@ -1,7 +1,7 @@
 dog_food_count = int(input())
-
 cat_food_count = int(input())
 
 total_food_price = dog_food_count * 2.50 + cat_food_count * 4
+output = f'{total_food_price} lv.'
 
-print(f'{total_food_price} lv.')
+print(output)
