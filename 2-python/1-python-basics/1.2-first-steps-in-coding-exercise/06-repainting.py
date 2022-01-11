1f63c4d5f02517a73318151245d75ef9ef09f560
erikivanov7 <advictoriam@abv.bg>
2022-01-11 17:22:09 +0200

    Modified sixth solution

--- 2-python/1-python-basics/1.2-first-steps-in-coding-exercise/06-repainting.py
+++ 2-python/1-python-basics/1.2-first-steps-in-coding-exercise/06-repainting.py
@@ -11,11 +11,11 @@ bags_price = 0.4
 total_nylon_price = (needed_nylon_sq_m + 2) * nylon_price_sq_m
 total_paint_price = needed_paint_liters * 1.1 * paint_price_liter
 total_paint_thinner_price = needed_paint_thinner_liters * paint_thinner_price_liter
-materials_total_price = total_nylon_price + \
+total_materials_price = total_nylon_price + \
                         total_paint_price + \
                         total_paint_thinner_price + \
                         bags_price
-workers_total_price = deadline_hours * (materials_total_price * 0.3)
-total_price_all = materials_total_price + workers_total_price
+total_workers_price = deadline_hours * (total_materials_price * 0.3)
+total_price_all = total_materials_price + total_workers_price
 
 print(total_price_all)
