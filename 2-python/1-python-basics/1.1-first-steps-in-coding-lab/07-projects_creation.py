76a640a262b398b52402e66c877fa96bac820aa1
erikivanov7 <advictoriam@abv.bg>
2022-01-11 19:36:56 +0200

    Modified seventh solution

--- 2-python/1-python-basics/1.1-first-steps-in-coding-lab/07-projects_creation.py
+++ 2-python/1-python-basics/1.1-first-steps-in-coding-lab/07-projects_creation.py
@@ -2,6 +2,5 @@ architect_name = input()
 projects_count = int(input())
 
 projects_creation_time = projects_count * 3
-output = f'The architect {architect_name} will need {projects_creation_time} hours to complete {projects_count} project/s.'
 
-print(output)
+print(f'The architect {architect_name} will need {projects_creation_time} hours to complete {projects_count} project/s.')
