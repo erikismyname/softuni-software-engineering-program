76d588066d624f0bf0265e790bfb89da6a78924e
erikivanov7 <advictoriam@abv.bg>
2022-01-11 15:28:08 +0200

    Modified fourth solution

--- 2-python/1-python-basics/1.2-first-steps-in-coding-exercise/04-vacation_books_list.py
+++ 2-python/1-python-basics/1.2-first-steps-in-coding-exercise/04-vacation_books_list.py
@@ -2,8 +2,8 @@ current_book_pages = int(input())
 pages_read_per_hour = int(input())
 deadline_days = int(input())
 
-total_hours = current_book_pages // pages_read_per_hour
+total_hours = current_book_pages / pages_read_per_hour
 
-hours_per_day = total_hours // deadline_days
+hours_per_day = total_hours / deadline_days
 
-print(hours_per_day)
+print(int(hours_per_day))
