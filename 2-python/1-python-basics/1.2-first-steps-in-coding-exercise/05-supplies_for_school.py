a930bb62c5f892e2e9389eed8270212a07797e15
erikivanov7 <advictoriam@abv.bg>
2022-01-11 17:16:25 +0200

    Modified fifth solution

--- 2-python/1-python-basics/1.2-first-steps-in-coding-exercise/05-supplies_for_school.py
+++ 2-python/1-python-basics/1.2-first-steps-in-coding-exercise/05-supplies_for_school.py
@@ -8,9 +8,9 @@ marker_package_price = 7.2
 board_detergent_liter_price = 1.2
 
 discount = (100 - discount_percent) / 100
-total_sum = (pens_packages_count * pens_package_price +
-             markers_packages_count * marker_package_price +
-             board_detergent_liters * board_detergent_liter_price) * \
-            discount
+total_price = (pens_packages_count * pens_package_price +
+               markers_packages_count * marker_package_price +
+               board_detergent_liters * board_detergent_liter_price) * \
+              discount
 
-print(total_sum)
+print(total_price)
