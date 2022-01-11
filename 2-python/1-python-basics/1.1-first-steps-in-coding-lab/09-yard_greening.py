25978c300843067846097a8ccc3bf0a6bfcb6426
erikivanov7 <advictoriam@abv.bg>
2022-01-11 19:32:40 +0200

    Modified ninth solution

--- 2-python/1-python-basics/1.1-first-steps-in-coding-lab/09-yard_greening.py
+++ 2-python/1-python-basics/1.1-first-steps-in-coding-lab/09-yard_greening.py
@@ -1,7 +1,8 @@
 total_square_m = float(input())
 
 total_square_m_price = total_square_m * 7.61
+total_square_m_price_discount = total_square_m_price * 0.82
+discount = total_square_m_price * 0.18
 
-print(f'The final price is: {total_square_m_price * 0.82} lv.')
-
-print(f'The discount is: {total_square_m_price * 0.18} lv')
+print(f'The final price is: {total_square_m_price_discount} lv.')
+print(f'The discount is: {discount} lv')
