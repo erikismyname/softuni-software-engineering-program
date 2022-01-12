0a388856baaeae55ec2eca4413d7286c8d52898c
erikivanov7 <advictoriam@abv.bg>
2022-01-12 23:14:55 +0200

    Added second solution

---
+++ 2-python/1-python-basics/2.2-conditional-statements-exercise/02-bonus_score.py
@@ -0,0 +1,18 @@
+num = int(input())
+
+bonus = 0
+
+if num <= 100:
+    bonus = 5
+elif num <= 1000:
+    bonus = num * 0.2
+else:
+    bonus = num * 0.1
+
+if num % 2 == 0:
+    bonus += 1
+
+if num % 10 == 5:
+    bonus += 2
+
+print(f'{bonus}\n{num + bonus}')
